5aa9f757430600453a76eb5c154c8c23f360a657
Marten Richter <marten.richter@tu-berlin.de>
2021-12-31 16:33:06 +0100

    Initial commit of authhandler

---
+++ src/server.js
@@ -0,0 +1,98 @@
+/*
+    Fails Components (Fancy Automated Internet Lecture System - Components)
+    Copyright (C)  2015-2017 (original FAILS), 
+                   2021- (FAILS Components)  Marten Richter <marten.richter@freenet.de>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import { createServer } from 'http'
+import { Server } from 'socket.io'
+import * as redis from 'redis'
+import { AuthConnection } from './authhandler.js'
+import { FailsJWTSigner } from '@fails-components/security'
+import { FailsConfig } from '@fails-components/config'
+
+const initServer = async () => {
+  const cfg = new FailsConfig()
+
+  // this should be read only replica
+  const redisclient = redis.createClient({
+    socket: { port: cfg.redisPort(), host: cfg.redisHost() },
+    password: cfg.redisPass()
+  })
+
+  const server = createServer()
+
+  const lecturesecurity = new FailsJWTSigner({
+    redis: redisclient,
+    type: 'lecture',
+    expiresIn: '10m',
+    secret: cfg.getKeysSecret()
+  })
+  const screensecurity = new FailsJWTSigner({
+    redis: redisclient,
+    type: 'screen',
+    expiresIn: '10m',
+    secret: cfg.getKeysSecret()
+  })
+
+  // may be move the io also inside the object, on the other hand, I can not insert middleware anymore
+
+  let cors = null
+
+  if (cfg.needCors()) {
+    cors = {
+      origin: cfg.getURL('web'),
+      methods: ['GET', 'POST']
+      // credentials: true
+    }
+  }
+
+  const ioIns = new Server(server, {
+    cors: cors,
+    path: '/auth.io',
+    serveClient: false
+  })
+  const authio = ioIns.of('/auth')
+
+  await redisclient.connect()
+
+  const nsconn = new AuthConnection({
+    redis: redisclient,
+    authio: authio,
+    signScreenJwt: screensecurity.signToken,
+    signNotepadJwt: lecturesecurity.signToken,
+    notepadhandlerURL: cfg.getURL('notepad'),
+    authhandlerURL: cfg.getURL('auth')
+  })
+
+  // no verification this is login, thus unauthorized!
+  // authio.use(authverifier.socketauthorize())
+  authio.on('connection', (socket) => {
+    nsconn.SocketHandlerAuth.bind(nsconn, socket)()
+  })
+
+  let port = cfg.getPort('auth')
+  if (port === 443) port = 8080 // we are in production mode inside a container
+
+  server.listen(port, cfg.getHost(), function () {
+    console.log(
+      'Failsserver auth listening at http://%s:%s',
+      server.address().address,
+      server.address().port
+    )
+  })
+}
+initServer()
